a8b1a9c395eeee9dd410602d63d2b7a81439b394
Mauro Cifuentes <mauro.cifuentes88@gmail.com>
2023-02-13 15:12:15 -0300

    Correccion

--- scriptjs.js
+++ scriptjs.js
@@ -1,12 +1,25 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+
   const cuadrados = document.querySelectorAll('.cuadrado');
+
   const body = document.querySelector('body');
 
-  function cambiarColor() {
-    body.style.backgroundColor = this.style.backgroundColor;
+
+
+  function cambiarColor(e) {
+
+	var color = document.defaultView.getComputedStyle(e.target).getPropertyValue('background-color')
+
+    body.style.setProperty('background-color', color);
+
   }
 
+
+
   for (let i = 0; i < cuadrados.length; i++) {
+
     cuadrados[i].addEventListener('click', cambiarColor);
+
   }
+
 });
